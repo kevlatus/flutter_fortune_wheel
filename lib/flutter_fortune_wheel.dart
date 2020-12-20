8e5383b1ea20b35d140ced4d112a4543a8399ec2
Kevin Latusinski <me@kevlatus.de>
2020-12-20 13:00:02 +0100

    feat: export random utilities from this package

--- lib/flutter_fortune_wheel.dart
+++ lib/flutter_fortune_wheel.dart
@@ -5,3 +5,4 @@ export 'package:flutter_fortune_wheel/src/fortune_widget.dart';
 export 'package:flutter_fortune_wheel/src/bar/bar.dart';
 export 'package:flutter_fortune_wheel/src/indicators/indicators.dart';
 export 'package:flutter_fortune_wheel/src/wheel/wheel.dart';
+export 'package:flutter_fortune_wheel/src/util.dart' show Fortune;
