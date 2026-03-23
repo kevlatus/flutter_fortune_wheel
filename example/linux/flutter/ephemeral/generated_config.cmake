9952d3689fbda1c84e0f558c62b6cf7eb0f079a5
Kevin Latusinski <me@kevlatus.de>
2026-03-23 09:27:49 +0100

    chore: add linux target

---
+++ example/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/kevlatus/dev/tools/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/kevlatus/dev/kevlatus/flutter_fortune_wheel/example" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/kevlatus/dev/tools/flutter"
+  "PROJECT_DIR=/home/kevlatus/dev/kevlatus/flutter_fortune_wheel/example"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZmYzN2JlZjYwMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZTRiOGRjYTNmMQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4x"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/kevlatus/dev/kevlatus/flutter_fortune_wheel/example/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/kevlatus/dev/kevlatus/flutter_fortune_wheel/example/lib/main.dart"
+)
